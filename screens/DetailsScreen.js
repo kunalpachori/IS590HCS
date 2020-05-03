e69d9f637aed2fd1ca5d4eb613a22d84dc07dbcc
kunalpachori <54906343+kunalpachori@users.noreply.github.com>
2020-05-03 17:58:01 -0500

    Updated DetailsScreen code as final draft

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,69 +1,115 @@
 import * as React from 'react';
-import {useState} from 'react';
-import { Container, Content, Card, Button, Text, CardItem, Left, View, Fab, Right, Icon, Body, Badge, List, ListItem } from 'native-base';
+import { useState } from 'react';
+import {
+  Container,
+  Content,
+  Card,
+  Button,
+  Text,
+  CardItem,
+  Left,
+  View,
+  Fab,
+  Right,
+  Icon,
+  Body,
+  Badge,
+  List,
+  Accordion,
+  ListItem,
+} from 'native-base';
 
 export default function DetailsScreen(props) {
-    const { request } = props.navigation.state.params;
-    const [setState] = useState(false);
-    return (
-      <Container>
-        <Content padder>
-          <Card>
-              <CardItem header>
-                <Left>
-                  <Body>
-                    <Text>{request.date}</Text>
-                    <Text note>{request.deliveryWindow}</Text>
-                    <Text note>{request.instructions}</Text>
-                  </Body>
-                </Left>
-              </CardItem>
+  const { request } = props.navigation.state.params;
+  const [state, setState] = useState(false);
+  return (
+    <Container>
+      <Content padder>
+        <Card>
+          <CardItem header>
+            <Icon active name="ios-cart" style={{ color: '#67c100' }} />
+            <Text>{'Delivery Date: '}</Text>
+            <Text>{request.date}</Text>
+          </CardItem>
 
-              <CardItem>
-                <Left>
-                  <Button onPress={() => props.navigation.push('Requests', {remove: true})} bordered small danger><Text>Cancel</Text></Button>
-                </Left>
-                <Right>
-                  <Button small primary><Icon active name="ios-call" /><Text>Call Helper</Text></Button>
-                </Right>
-              </CardItem>
+          <CardItem footer style={{ justifyContent: 'space-evenly' }}>
+            <Left>
+              <Text>Status:</Text>
+              <Badge warning>
+                <Text>{request.status}</Text>
+              </Badge>
+            </Left>
+            <Button small primary transparent>
+              <Icon iconRight="10" active name="ios-call" />
+            </Button>
+            <Text primary>{'Helper'}</Text>
+          </CardItem>
+        </Card>
 
-              <CardItem footer>
-                <Text>Status:</Text>
-                <Badge warning><Text>{request.status}</Text></Badge>
-              </CardItem>
-            </Card>
+        <Card>
+          <CardItem>
+            <Icon active name="md-car" style={{ color: '#67c100' }} />
+            <Text>{'Deliver to '}</Text>
+            <Text>{request.address}</Text>
+          </CardItem>
+        </Card>
 
-            <List>
-            {
-              request.items.map(item => (
-                <ListItem key={item}>
-                  <Text>{item}</Text>
-                </ListItem>
-              ))
-            }
-            </List>
-        </Content>
-        <View style={{ flex: 1 }}>
-          <Fab
-            active={true}
-            direction="up"
-            containerStyle={{ }}
-            style={{ backgroundColor: '#5067FF' }}
-            position="bottomRight"
-            onPress={() => setState(true)}>
-            <Icon name="share" />
-            <Button style={{ backgroundColor: '#34A34F' }}>
-              <Icon name="logo-whatsapp" />
-            </Button>
-            <Button style={{ backgroundColor: '#3B5998' }}>
-              <Icon name="logo-facebook" />
-            </Button>
-            <Button disabled style={{ backgroundColor: '#DD5144' }}>
-              <Icon name="mail" />
-            </Button>
-          </Fab>
-        </View>
-      </Container>
-    );
+        <Card>
+          <CardItem>
+            <Icon active name="ios-person" style={{ color: '#67c100' }} />
+            <Text>{request.name}</Text>
+          </CardItem>
+        </Card>
+
+        <Card>
+          <CardItem>
+            <Icon active name="ios-calendar" style={{ color: '#67c100' }} />
+            <Text>{request.deliveryWindow}</Text>
+          </CardItem>
+        </Card>
+
+        <Card>
+          <CardItem>
+            <Icon
+              active
+              name="ios-information-circle-outline"
+              style={{ color: '#67c100' }}
+            />
+            <Text>{'Instructions: '}</Text>
+            <Text>{request.instructions}</Text>
+          </CardItem>
+        </Card>
+
+        <List>
+          <ListItem itemDivider>
+            <Text>{'Items'}</Text>
+          </ListItem>
+          {request.items.map(item => (
+            <ListItem key={item}>
+              <Text>{item}</Text>
+            </ListItem>
+          ))}
+        </List>
+      </Content>
+
+      <Fab
+        active={state}
+        direction="up"
+        containerStyle={{}}
+        style={{ backgroundColor: '#5067FF' }}
+        position="bottomRight"
+        onPress={() => (state ? setState(false) : setState(true))}>
+        <Icon name="share" />
+        <Button style={{ backgroundColor: '#34A34F' }}>
+          <Icon name="logo-whatsapp" />
+        </Button>
+        <Button style={{ backgroundColor: '#3B5998' }}>
+          <Icon name="logo-facebook" />
+        </Button>
+        <Button disabled style={{ backgroundColor: '#DD5144' }}>
+          <Icon name="mail" />
+        </Button>
+      </Fab>
+    </Container>
+  );
 }
